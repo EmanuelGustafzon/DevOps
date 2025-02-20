15436fa3053369d7a9872ef06a402d4a5e1bb7f8
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2025-02-20 17:49:14 +0100

    Add project files.

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using consoleApplication;
+
+namespace Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        int ten = CountToTen.Count();
+
+        Assert.Equal(10, ten);
+    }
+}
---
+++ consoleApplication/CountToTen.cs
@@ -0,0 +1,14 @@
+﻿namespace consoleApplication;
+
+public static class CountToTen
+{
+    public static int Count()
+    {
+        int count = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            count++;
+        }
+        return count;
+    }
+}
